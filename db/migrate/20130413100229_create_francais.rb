9d1ae10bf6e66d62a6343b255234707db260dbbe
ThierryD <thierryvianney@yahoo.fr>
2015-06-13 22:09:13 +0200

    make it better win7

--- db/migrate/20130413100229_create_francais.rb
+++
@@ -1,9 +0,0 @@
-class CreateFrancais < ActiveRecord::Migration
-  def change
-    create_table :francais do |t|
-      t.string :mot
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130413100241_create_anglais.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnglais < ActiveRecord::Migration
-  def change
-    create_table :anglais do |t|
-      t.string :mot
-
-      t.timestamps
-    end
-  end
-end
